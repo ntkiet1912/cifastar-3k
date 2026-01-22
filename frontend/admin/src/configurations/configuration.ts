510c3f6711e1ee46522f68a3b53a884495e0ce10
ntkiet1912 <pupgkiet@gmail.com>
2026-01-23 02:46:02 +0700

    fix

--- frontend/admin/src/configurations/configuration.ts
+++ frontend/admin/src/configurations/configuration.ts
@@ -1,6 +1,9 @@
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api/theater-mgnt";
+
 export const CONFIG = {
-  API: "http://localhost:8080/api/theater-mgnt",
-}
+  API: API_BASE_URL,
+};
 export const API = {
   LOGIN: "/auth/admin/login",
   FORGOT_PASSWORD: "/auth/forgot-password",
