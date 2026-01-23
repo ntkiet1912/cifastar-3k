c7ee852d47946606ccdcb004c28979639a245fe6
KietTuan <theonlytruth25012005@gmail.com>
2026-01-23 18:11:02 +0700

    fix vite base path

--- frontend/admin/vite.config.ts
+++ frontend/admin/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
-    base: env.VITE_BASE_PATH || '/admin',
+    base: env.VITE_BASE_PATH || '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
