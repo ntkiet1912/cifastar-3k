5a24b71948e2bef1064e4a4047e1af2574e6236f
ntkiet1912 <pupgkiet@gmail.com>
2026-01-23 14:32:28 +0700

    fix 1 ty lan

--- frontend/admin/src/configurations/configuration.ts
+++ frontend/admin/src/configurations/configuration.ts
@@ -1,5 +1,8 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api/theater-mgnt";
+  import.meta.env.VITE_API_BASE_URL ||
+  (import.meta.env.PROD
+    ? "/api/theater-mgnt"
+    : "http://localhost:8080/api/theater-mgnt");
 
 export const CONFIG = {
   API: API_BASE_URL,
